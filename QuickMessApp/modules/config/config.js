76eadaf6024135733d8bef4f7644b644535cace4
tiosca <vitalie.nanu@gmail.com>
2023-07-11 13:37:08 +0300

    Removed the API key

--- QuickMessApp/modules/config/config.js
+++ QuickMessApp/modules/config/config.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
-process.env.OPENAI_API_KEY = process.env.OPENAI_API_KEY || "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";
+process.env.OPENAI_API_KEY = process.env.OPENAI_API_KEY || "YOUR-API-KEY";
 
 module.exports = {
     apiUrl: (process.env.NODE_ENV === "Development")? "http://172.19.0.11:5123/" : "http://localhost:5123/",
